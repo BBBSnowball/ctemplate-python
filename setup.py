5fb666c83f3e8ffd80892e18421dbe3a7d787053
calvin <calvin@f9221b96-bf33-0410-bc82-453897b809b3>
2006-09-22 15:31:07 +0000

    bump up version
    git-svn-id: http://python-ctemplate.googlecode.com/svn/trunk@23 f9221b96-bf33-0410-bc82-453897b809b3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ myname = "Bastian Kleineidam"
 myemail = "calvin@debian.org"
 
 setup (name = 'python-ctemplate',
-       version = '0.4',
+       version = '0.6',
        description = 'Python wrapper for the ctemplate library',
        author = myname,
        author_email = myemail,
